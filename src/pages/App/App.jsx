27a26feef3a0574e4c0644fc651ed0ec61416c57
theannagreen <theannalgreen@gmail.com>
2024-05-20 08:38:31 -0400

    moved files to main branch

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+// import { Routes, Route } from 'react-router-dom';
+import { getUser } from '../../utilities/users-service';
+import './App.css';
+import AuthPage from '../AuthPage/AuthPage';
+import NavBar from '../../components/NavBar/NavBar';
+
+export default function App() {
+  const [user, setUser] = useState(getUser());
+
+  return (
+    <main className="App">
+      { user ?
+          <>
+            <NavBar user={user} setUser={setUser} />
+            {/* <Routes>
+              <Route path="" element={} />
+              <Route path="" element={} />
+            </Routes> */}
+          </>
+          :
+          <AuthPage setUser={setUser} />
+      }
+    </main>
+  );
+}
