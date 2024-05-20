d4b8c7b5f8e9d61997e4d43c45193bbbdfa75523
Sagespellman <sagespellman@outlook.com>
2024-05-20 17:00:56 -0400

    configuring files to work with macOS system due to errors

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -12,13 +12,12 @@ export default async function sendRequest(url, method = 'GET', payload = null) {
   if (token) {
     // Need to add an Authorization header
     // Use the Logical OR Assignment operator
-    options.headers ||= {};
+    options.headers = options.headers || {};
     // Older approach
     // options.headers = options.headers || {};
     options.headers.Authorization = `Bearer ${token}`;
   }
   const res = await fetch(url, options);
-  // if res.ok is false then something went wrong
   if (res.ok) return res.json();
   throw new Error('Bad Request');
 }
