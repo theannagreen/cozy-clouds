a209a34453a014f4a6a4d7fcaf228441d67f81d6
theannagreen <theannalgreen@gmail.com>
2024-05-20 17:19:29 -0400

    added weather data to user sign-up and login functionalities

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,10 +1,12 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const User = require('../../models/user');
+const fetchWeatherData = require('../../models/weather');
 
 module.exports = {
   create,
   login,
+  getWeather,
 };
 
 async function create(req, res) {
@@ -12,20 +14,22 @@ async function create(req, res) {
     // Add the user to the db
     const user = await User.create(req.body);
     const token = createJWT(user);
-    res.json(token);
+    const weatherData = await fetchWeatherData('New York'); // made NY the default but can be changed 
+    res.json({ token, weatherData });
   } catch (err) {
-    res.status(400).json(err);
+    res.status(400).json(' Bad Credentials');
   }
 }
 
 async function login(req, res) {
   try {
-    const user = await User.findOne({email: req.body.email});
+    const user = await User.findOne({ email: req.body.email });
     if (!user) throw new Error();
     const match = await bcrypt.compare(req.body.password, user.password);
     if (!match) throw new Error();
     const token = createJWT(user);
-    res.json(token);
+    const weatherData = await fetchWeatherData('New York'); // fetching data from default city 
+    res.json({ token, weatherData });
   } catch (err) {
     res.status(400).json('Bad Credentials');
   }
